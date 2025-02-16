f8d123e5453440c42e24e73f559f34d6b1744647
nitu5-lab <pas081bct021@wrc.edu.np>
2025-02-17 02:05:04 +0545

    Add files via upload
    to store the even and odd elements in different array

---
+++ 13.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
+int main() {
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    int greatestCommonDivisor = gcd(num1, num2);
+    int leastCommonMultiple = lcm(num1, num2);
+
+    printf("GCD of %d and %d is %d\n", num1, num2, greatestCommonDivisor);
+    printf("LCM of %d and %d is %d\n", num1, num2, leastCommonMultiple);
+
+    return 0;
+}
