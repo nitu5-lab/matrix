4f85f4f90d5dfbc6fbcb5b2731f6c59d05260424
nitu5-lab <pas081bct021@wrc.edu.np>
2025-02-17 13:13:13 +0545

    Add files via upload

---
+++ l2q6c.c
@@ -0,0 +1,17 @@
+//wap to check odd & even using % operator
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a; 
+	printf("Enter a no ");
+	scanf("%d",&a);
+if (a%2==0)
+printf("Even");
+else 
+printf("Odd");
+
+	return 0;
+	getch();
+}
