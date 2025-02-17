71a2a9473c0b4176f8fb578839c666ea283c556e
nitu5-lab <pas081bct021@wrc.edu.np>
2025-02-17 12:57:55 +0545

    Add files via upload

---
+++ l1q4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,s=0;
+	printf("Enter 2 no ");
+	scanf("%d %d",&a,&b);
+	s=a*b
+	;
+	printf("Product is %d",s);
+	getch();
+	return 0;
+}
